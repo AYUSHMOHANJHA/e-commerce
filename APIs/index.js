bd3a3cff77efd283bb988b2bd2c5fe9a8e8f9dbb
AYUSHMOHANJHA <ayushjha06@gmail.com>
2022-10-31 23:45:38 +0530

    Authentication, Register Login and JWT intregrated

--- APIs/index.js
+++ APIs/index.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
-
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -16,6 +16,7 @@ mongoose
 });
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
  
 
